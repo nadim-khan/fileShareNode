aa034d3152dd696089387f8c36aae6a9e2105a1f
nadim-khan <khan.nadim58@gmail.com>
2021-04-09 21:22:24 +0530

    updated check

--- routes/download.js
+++ routes/download.js
@@ -10,6 +10,7 @@ router.get('/:uuid', async (req, res) => {
    } 
    const response = await file.save();
    const filePath = `${__dirname}/..${file.path}`;
+   console.log('---------------------->',filePath)
    res.download(filePath);
 });
 
